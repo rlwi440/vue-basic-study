1f5e79cd97876d99cca49f56e1659c91f4c14d61
rlwi440 <rlwi440@naver.com>
2021-10-24 23:15:32 +0900

    feat:login 컴포넌트 개발 및 유효성검사

--- component/vue-form/src/App.vue
+++ component/vue-form/src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="username">ID:</label>
+        <input
+          type="text"
+          id="username"
+          v-model="username"
+          class="username-input "
+          :class="{ error: isError }"
+        />
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit" :disabled="!isUsernameVaild">로그인</button>
+    </form>
+    <p v-if="isError">올바르지 않은 ID입니다.</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(email).toLowerCase())
+}
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      username: "",
+      password: "",
+      isError: false
+    }
+  },
+  computed: {
+    isUsernameVaild() {
+      return validateEmail(this.username)
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log("login")
+      this.isError = true
+      // this.initFrom()
+    },
+    initFrom() {
+      this.username = ""
+      this.password = ""
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.username-input {
+  outline: none;
+}
+
+.username-input.error {
+  border: 1px solid red;
 }
 </style>
